4b0d1f068043e82f4f9283c8f5d4746b58862d41
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-08-02 11:00:46 -0600

    WIP attempt to get auto logout after inactivity

--- src/components/app.js
+++ src/components/app.js
@@ -7,52 +7,64 @@ import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
 import {refreshAuthToken} from '../actions/auth';
+import {clearAuth} from '../actions/auth';
 
 export class App extends React.Component {
-    componentDidUpdate(prevProps) {
-        if (!prevProps.loggedIn && this.props.loggedIn) {
-            // When we are logged in, refresh the auth token periodically
-            this.startPeriodicRefresh();
-        } else if (prevProps.loggedIn && !this.props.loggedIn) {
-            // Stop refreshing when we log out
-            this.stopPeriodicRefresh();
-        }
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  logOut() {
+    console.log('should logout');
+    this.props.dispatch(clearAuth());
+    //clearAuthToken();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      10 * 60 * 1000 // Ten minutes
+    );
+
+    this.logoutInactive = setInterval(
+      () => this.logOut(),
+      5 * 60 * 1000 // 5 min
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
     }
 
-    componentWillUnmount() {
-        this.stopPeriodicRefresh();
-    }
+    clearInterval(this.refreshInterval);
+  }
 
-    startPeriodicRefresh() {
-        this.refreshInterval = setInterval(
-            () => this.props.dispatch(refreshAuthToken()),
-            60 * 60 * 1000 // One hour
-        );
-    }
-
-    stopPeriodicRefresh() {
-        if (!this.refreshInterval) {
-            return;
-        }
-
-        clearInterval(this.refreshInterval);
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <HeaderBar />
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/register" component={RegistrationPage} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <HeaderBar />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={RegistrationPage} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    hasAuthToken: state.auth.authToken !== null,
-    loggedIn: state.auth.currentUser !== null
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
 });
 
 // Deal with update blocking - https://reacttraining.com/react-router/web/guides/dealing-with-update-blocking
